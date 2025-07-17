cb6e2e5bb831a163c3dfd4a51ae1e3c7b527f040
dmtricanaday <xxdmtrixx99@gmail.com>
2025-07-16 20:51:47 -0400

    update

--- scripts/index.js
+++ scripts/index.js
@@ -1,35 +1,26 @@
 const editProfileButton = document.querySelector(".profile__edit-button");
 const editProfileModal = document.querySelector("#edit-profile-modal");
-const editProfileCloseButton = editProfileModal
-  ? editProfileModal.querySelector(".modal__close-button")
-  : null;
-const newPostModal = document.querySelector("#new-post-modal");
-const newPostCloseButton = newPostModal
-  ? newPostModal.querySelector(".modal__close-button")
-  : null;
+const editProfileCloseButton = editProfileModal.querySelector(".modal__close-button");
+
+
 const newPostButton = document.querySelector(".profile__add-button");
+const newPostModal = document.querySelector("#new-post-modal");
+const newPostCloseButton = newPostModal.querySelector(".modal__close-button");
 
-// Add event listeners only if elements exist
-if (editProfileButton && editProfileModal) {
-  editProfileButton.addEventListener("click", function () {
-    editProfileModal.classList.add("modal_is-opened");
-  });
-}
 
-if (editProfileCloseButton && editProfileModal) {
-  editProfileCloseButton.addEventListener("click", function () {
-    editProfileModal.classList.remove("modal_is-opened");
-  });
-}
+editProfileButton.addEventListener("click", function () {
+  editProfileModal.classList.add("modal_is-opened");
+});
 
-if (newPostButton && newPostModal) {
-  newPostButton.addEventListener("click", function () {
-    newPostModal.classList.add("modal_is-opened");
-  });
-}
+editProfileCloseButton.addEventListener("click", function () {
+  editProfileModal.classList.remove("modal_is-opened");
+});
 
-if (newPostCloseButton && newPostModal) {
-  newPostCloseButton.addEventListener("click", function () {
-    newPostModal.classList.remove("modal_is-opened");
-  });
-}
+
+newPostButton.addEventListener("click", function () {
+  newPostModal.classList.add("modal_is-opened");
+});
+
+newPostCloseButton.addEventListener("click", function () {
+  newPostModal.classList.remove("modal_is-opened");
+});
