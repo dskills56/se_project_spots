bbf03bd34d01f31c783c1004dcd8ca095d64da49
dmtricanaday <xxdmtrixx99@gmail.com>
2025-07-16 20:57:49 -0400

    update2

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,12 @@
 const editProfileButton = document.querySelector(".profile__edit-button");
 const editProfileModal = document.querySelector("#edit-profile-modal");
-const editProfileCloseButton = editProfileModal.querySelector(".modal__close-button");
-
+const editProfileCloseButton = editProfileModal.querySelector(
+  ".modal__close-button"
+);
 
 const newPostButton = document.querySelector(".profile__add-button");
 const newPostModal = document.querySelector("#new-post-modal");
 const newPostCloseButton = newPostModal.querySelector(".modal__close-button");
-
 
 editProfileButton.addEventListener("click", function () {
   editProfileModal.classList.add("modal_is-opened");
@@ -15,7 +15,6 @@ editProfileButton.addEventListener("click", function () {
 editProfileCloseButton.addEventListener("click", function () {
   editProfileModal.classList.remove("modal_is-opened");
 });
-
 
 newPostButton.addEventListener("click", function () {
   newPostModal.classList.add("modal_is-opened");
